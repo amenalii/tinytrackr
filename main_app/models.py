66a5d9420cf3100ac866da5972384aabdec2cf16
amenalii <ummeamen@gmail.com>
2025-06-04 20:17:00 -0500

    Created app models and ran migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('NB', 'Non-binary'),
+    ('O', 'Other'),
+    ('P', 'Prefer not to say'),
+]
+
+class Child(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    child = models.ForeignKey(Child, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    activity_type = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.activity_type} - {self.child.name} on {self.date}"
--- tinytrackr/settings.py
+++ tinytrackr/settings.py
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'tinytrackr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tinytrackr',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
