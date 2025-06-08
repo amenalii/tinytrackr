0682b94a951a5f3cef351ea8e21828348b0d5edc
amenalii <ummeamen@gmail.com>
2025-06-08 18:07:31 -0500

    Created a dashboard view and superuser

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Child, Activity
 
 # Register your models here.
+admin.site.register(Child)
+admin.site.register(Activity)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Child, Activity
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def dashboard(request):
+    children = Child.objects.filter(user=request.user)
+    return render(request, 'main_app/dashboard.html', {'children': children})
